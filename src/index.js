80da089b209da858ffdf9d6d128498608e1411fb
Suryo Sasono <suryo.ssn11@gmail.com>
2022-07-04 08:00:49 +0700

    menambahkan file

---
+++ src/index.js
@@ -0,0 +1,8 @@
+require('file-loader?name=[name].[ext]!../public/index.html')
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import NoteApp from './components/NoteApp';
+import './styles/style.css';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<NoteApp />);
